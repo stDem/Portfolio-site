e405366efe68350db6318449354db6294c4a4dd4
stDem <61629062+stDem@users.noreply.github.com>
2024-11-08 11:00:28 +0100

    v1.0

---
+++ mysite/src/components/Header.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faLinkedin,
+  faWhatsapp,
+  faGoogle,
+} from "@fortawesome/free-brands-svg-icons";
+import { Box, HStack } from "@chakra-ui/react";
+
+const socials = [
+  {
+    icon: faEnvelope,
+    url: "mailto:stxdem@gmail.com",
+  },
+  {
+    icon: faGithub,
+    url: "https://github.com/stDem?tab=repositories",
+  },
+  {
+    icon: faLinkedin,
+    url: "https://www.linkedin.com/in/anastasiia-demidova-0aa44a329/",
+  },
+  {
+    icon: faWhatsapp,
+    url: "https://api.whatsapp.com/send?phone=491628645712",
+  },
+  {
+    icon: faGoogle,
+    url: "https://drive.google.com/drive/u/2/folders/1VPCcvftPZAneZu0BB-xQq0Upe2h9qmER",
+  },
+];
+
+const Header = () => {
+  const headerRef = useRef(null); 
+ 
+  useEffect(() => { 
+    let prevScrollPos = window.scrollY; 
+  
+    const handleScroll = () => { 
+      const currentScrollPos = window.scrollY; 
+      const headerElement = headerRef.current; 
+      if (!headerElement) { 
+        return; 
+      } 
+      if (prevScrollPos > currentScrollPos) { 
+        headerElement.style.transform = "translateY(0)"; 
+      } else { 
+        headerElement.style.transform = "translateY(-200px)"; 
+      } 
+      prevScrollPos = currentScrollPos; 
+    } 
+    window.addEventListener('scroll', handleScroll) 
+  
+    return () => { 
+      window.removeEventListener('scroll', handleScroll) 
+    } 
+  }, []); 
+  
+  const handleClick = (anchor) => () => { 
+    const id = `${anchor}-section`; 
+    const element = document.getElementById(id); 
+    if (element) { 
+      element.scrollIntoView({ 
+        behavior: "smooth", 
+        block: "start", 
+      }); 
+    } 
+  }; 
+
+  return (
+    <Box
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      translateY={0}
+      transitionProperty="transform"
+      transitionDuration=".3s"
+      transitionTimingFunction="ease-in-out"
+      backgroundColor="#18181b"
+      ref={headerRef} 
+    >
+      <Box color="white" maxWidth="1280px" margin="0 auto">
+        <HStack
+          px={16}
+          py={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <nav>
+            <HStack spacing={8}>
+              {
+                socials.map((social)=>
+                  <a href={social.url} key={social.url}>
+                    <FontAwesomeIcon icon={social.icon} size="2x" />
+                  </a>
+                )
+              }
+            </HStack>
+          </nav>
+          <nav>
+            <HStack spacing={8}>
+              <a href="#projects" onClick={handleClick("projects")}>Projects</a>
+              <a href="#contactme" onClick={handleClick("contactme")}>Contact Me</a>
+            </HStack>
+          </nav>
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+export default Header;
