e652bac33cc05c4b04b2fbd1fd710813647e5744
stDem <61629062+stDem@users.noreply.github.com>
2024-11-08 16:57:41 +0100

    v1.0 without node_modules

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import Header from "./components/Header";
+import LandingSection from "./components/LandingSection";
+import ProjectsSection from "./components/ProjectsSection";
+import ContactMeSection from "./components/ContactMeSection";
+import Footer from "./components/Footer";
+import { AlertProvider } from "./context/alertContext";
+import Alert from "./components/Alert";
+
+function App() {
+  return (
+    <div className="container">
+      <ChakraProvider>
+      <AlertProvider>
+        <main>
+          <Header />
+          <LandingSection />
+          <ProjectsSection />
+          <ContactMeSection />
+          <Footer />
+          <Alert />
+        </main>
+      </AlertProvider>
+    </ChakraProvider>
+    </div>
+  
+  );
+}
+
+export default App;
